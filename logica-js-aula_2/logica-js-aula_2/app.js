70474daae9cb847edfa992994822d4b6c6a6a8b5
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-07-04 21:16:32 -0300

    atualização

--- logica-js-aula_2/logica-js-aula_2/app.js
+++ logica-js-aula_2/logica-js-aula_2/app.js
@@ -1,12 +1,13 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 // Enquanto (while) o chute for diferente(!=) do número secreto execute esse bloco de códigos
 while(chute != numeroSecreto){
     // solicitando o número do chute através do prompt e guardando no espaço da memória que tem o nome chute
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
 
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
