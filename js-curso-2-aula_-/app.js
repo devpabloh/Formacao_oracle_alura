2182ba9c2041409aa535284b2bcc890c60ccf40f
Dev. Pablo H. <devpabloh@gmail.com>
2024-07-07 00:15:14 -0300

    att

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -5,5 +5,6 @@ let paragrafo = document.querySelector("p");
 paragrafo.innerHTML = "Escolha um número entre 1 e 10";
 
 function verificarChute(){
-    console.log(" está funcionando!")
+    return console.log(" está funcionando!")
+
 }
