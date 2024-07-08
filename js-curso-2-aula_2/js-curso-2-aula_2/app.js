0ed41031af6e4ab299c7167dca8cfdb46c2f4d8b
Dev. Pablo H. <devpabloh@gmail.com>
2024-07-08 09:40:43 -0300

    atualização

--- js-curso-2-aula_2/js-curso-2-aula_2/app.js
+++ js-curso-2-aula_2/js-curso-2-aula_2/app.js
@@ -10,7 +10,13 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Você acertou!')
+    }else if (chute > numeroSecreto){
+        exibirTextoNaTela('h1', 'O número é menor')
+    }else(
+        exibirTextoNaTela('h1', 'O número é maior')
+    )
 }
 
 function gerarNumeroAleatorio() {
